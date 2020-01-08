643ab17a8cd9aedb7416c84aee1f6bfe821d379f
arese221 <59662128+arese221@users.noreply.github.com>
2020-01-08 12:15:55 -0600

    Initial commit

---
+++ Assets/BallControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    public float moveStrength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.forward * moveStrength);
+        }
+        if (Input.GetKey("a"))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.left * moveStrength);
+        }
+        if (Input.GetKey("s"))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.back * moveStrength);
+        }
+        if (Input.GetKey("d"))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.right * moveStrength);
+        }
+    }
+}
---
+++ Assets/GoalLevel3.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalLevel3 : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Ball")
+        {
+            Debug.Log("You beat level 3! GoodJob!");
+            SceneManager.LoadScene("Level 4");
+        }
+    }
+}
